ee5664691439ebf7b801691daa7728a2b5776167
royrajan95 <84740586+royrajan95@users.noreply.github.com>
2023-02-04 22:57:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "5786986338:AAFZk_J25RrHeRtTPQ5-L_zsgthVJK88X1g")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "5503927"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "6f3a051b5da7f5b499cde019d273fca1")
 
 STRING = os.environ.get("STRING", "")
 
