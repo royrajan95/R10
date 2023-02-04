090af64f219fc1a88404301b66ff1dd90efb8622
royrajan95 <84740586+royrajan95@users.noreply.github.com>
2023-02-04 23:04:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,9 +17,9 @@ import os
 
 CHANNEL = os.environ.get('CHANNEL', "")
 STRING = os.environ.get("STRING", "")
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
+ADMIN = int(os.environ.get("ADMIN", 773325066))
 bot_username = os.environ.get("BOT_USERNAME","GangsterBaby_renamer_BOT")
-log_channel = int(os.environ.get("LOG_CHANNEL", ""))
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1001658377071"))
 token = os.environ.get('TOKEN', '')
 botid = token.split(':')[0]
 FLOOD = 500
